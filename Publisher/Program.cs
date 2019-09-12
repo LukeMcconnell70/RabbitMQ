4b3bcb1f4bde94ccfc41478c481e9b51602f4e65
lmcconnell70 <lmcconnell70@unm.edu>
2019-09-12 11:20:55 -0600

    initial commit

---
+++ Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=S1020890"))
+            {
+                var input = "";
+                Console.WriteLine("Enter a message. 'Quit' to quit.");
+                while ((input = Console.ReadLine()) != "Quit")
+                {
+                    bus.Publish(new TextMessage
+                    {
+                        Text = input
+                    });
+                }
+            }
+        }
+    }
+}
